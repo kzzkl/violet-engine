090b69c6c407bd4a6d920fd3203a593c2efd28b1
kzzkl <1433260379@qq.com>
2022-03-01 12:11:36 +0800

    Add plugin interface and unit tests

---
+++ engine/core/include/plugin.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "core_exports.hpp"
+#include "plugin_interface.hpp"
+#include <memory>
+#include <string_view>
+
+namespace ash::core
+{
+class dynamic_library
+{
+public:
+    virtual ~dynamic_library() = default;
+
+    virtual bool load(std::string_view path) = 0;
+    virtual void unload() = 0;
+
+    virtual void* find_symbol(std::string_view name) = 0;
+};
+
+class CORE_API plugin
+{
+public:
+    using version = ash::core::external::plugin_version;
+
+public:
+    plugin();
+    virtual ~plugin();
+
+    bool load(std::string_view path);
+    void unload();
+
+    std::string_view get_name() const { return m_name; }
+    version get_version() const { return m_version; }
+
+protected:
+    void* find_symbol(std::string_view name);
+
+    virtual bool do_load() { return true; }
+    virtual void do_unload() {}
+
+private:
+    std::string m_name;
+    version m_version;
+
+    std::unique_ptr<dynamic_library> m_library;
+};
+} // namespace ash::core
---
+++ tests/plugin/hello_plugin/source/hello.cpp
@@ -0,0 +1,21 @@
+#include "plugin_interface.hpp"
+#include <cstring>
+
+extern "C"
+{
+    PLUGIN_API ash::core::external::plugin_info get_plugin_info()
+    {
+        using namespace ash::core::external;
+        plugin_info info = {};
+
+        char name[] = "hello-plugin";
+        memcpy(info.name, name, sizeof(name));
+
+        info.version.major = 1;
+        info.version.minor = 0;
+
+        return info;
+    }
+
+    PLUGIN_API int hello_add(int a, int b) { return a + b; }
+}
